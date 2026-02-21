f09680ea9ee4550b91b86475dcef4d5bfb9f7ef3
levianKeyal <80283434+levianKeyal@users.noreply.github.com>
2026-02-21 13:05:38 -0600

    first round test. 2 decimals

--- Assets/Scripts/RoundUpTest.cs
+++ Assets/Scripts/RoundUpTest.cs
@@ -16,6 +16,12 @@ public class RoundUpTest : MonoBehaviour
     [SerializeField]
     private TMP_Text m_Messages;
 
+    public bool round1decimal;
+    public bool round2decimal;
+    public bool round3decimal;
+    public bool round4decimal;
+    public bool round5decimal;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -27,11 +33,11 @@ public class RoundUpTest : MonoBehaviour
     {
         number1 = Random.Range(0f, 100f);
         float truncated1 = Mathf.Floor(number1 * 100f) / 100f;
-        m_number1.text = truncated1.ToString("F2",CultureInfo.InvariantCulture);
+        m_number1.text = truncated1.ToString(CultureInfo.InvariantCulture);
 
         number2 = Random.Range(0f, 100f);
         float truncated2 = Mathf.Floor(number2 * 100f) / 100f;
-        m_number2.text = truncated2.ToString("F2", CultureInfo.InvariantCulture);
+        m_number2.text = truncated2.ToString(CultureInfo.InvariantCulture);
 
         answer = truncated1 + truncated2;
         Debug.Log(answer);
@@ -39,13 +45,28 @@ public class RoundUpTest : MonoBehaviour
 
     public void CheckForCorrectAnswer()
     {
+        float correctSum = answer;
+        float expected = Mathf.Round(correctSum * 10f) / 10f;
+
         float value;
 
         if(float.TryParse (m_answer.text, NumberStyles.Float, CultureInfo.InvariantCulture, out value))
         {
             Debug.Log("Valor ingresado: " + value);
-        }
+            float userRounded = Mathf.Round(value * 10f) / 10f;
 
+            if(Mathf.Approximately (expected, userRounded))
+            {
+                m_Messages.text = ("Respuesta Correcta!");
+                Debug.Log("Correct answer");
+            }
+            else
+            {
+                m_Messages.text = ("Respuesta Incorrecta!");
+                Debug.Log("Incorrect answer");
+            }
+        }
+        /*
         if(value == answer)
         {
             m_Messages.text = ("Respuesta Correcta!");
@@ -55,7 +76,7 @@ public class RoundUpTest : MonoBehaviour
         {
             m_Messages.text = ("Respuesta Incorrecta!");
             Debug.Log("Incorrect answer");
-        }
+        }*/
     }
     public void Reset()
     {
