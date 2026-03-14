c181dd22b86e1757bb920cd59644a81a05eaf59a
levianKeyal <80283434+levianKeyal@users.noreply.github.com>
2026-03-14 16:31:12 -0600

    Build Corrections

--- Assets/_Scripts/MathExercise.cs
+++ Assets/_Scripts/MathExercise.cs
@@ -1,6 +1,5 @@
-using System;
-using System.Globalization;
 using UnityEngine;
+using System;
 
 public class MathExercise
 {
@@ -8,92 +7,124 @@ public class MathExercise
     public decimal Number2 { get; private set; }
     public decimal Answer { get; private set; }
 
-    OperationType operation;
-
-    const int MAX_ATTEMPTS = 40;
+    static readonly decimal[] Pow10 =
+    {
+        1m,
+        10m,
+        100m,
+        1000m,
+        10000m,
+        100000m,
+        1000000m
+    };
 
     public MathExercise(int min, int max, OperationType operation)
     {
-        this.operation = operation;
+        switch (operation)
+        {
+            case OperationType.Add:
+                GenerateAddition(min, max);
+                break;
 
-        if (operation == OperationType.Divide)
-            GenerateSmartDivision(min, max);
-        else
-            GenerateSmartExercise(min, max);
+            case OperationType.Subtract:
+                GenerateSubtraction(min, max);
+                break;
+
+            case OperationType.Multiply:
+                GenerateMultiplication(min, max);
+                break;
+
+            case OperationType.Divide:
+                GenerateSmartDivision(min, max);
+                break;
+        }
     }
 
-    void GenerateSmartExercise(int min, int max)
+    decimal GenerateRandomDecimal(int min, int max)
     {
-        int attempts = 0;
+        int decimals = UnityEngine.Random.Range(0, 6);
+        decimal multiplier = Pow10[decimals];
 
-        while (attempts < MAX_ATTEMPTS)
-        {
-            decimal n1 = GenerateRandomDecimal(min, max);
-            decimal n2 = GenerateRandomDecimal(min, max);
+        int minInt = (int)(min * multiplier);
+        int maxInt = (int)(max * multiplier);
 
-            decimal result = Calculate(n1, n2);
+        int randomInt = UnityEngine.Random.Range(minInt, maxInt);
 
-            if (IsValidResult(result))
-            {
-                Number1 = n1;
-                Number2 = n2;
-                Answer = result;
+        return randomInt / multiplier;
+    }
 
-                Debug.Log($"Generated: {Number1} {GetSymbol()} {Number2} = {Answer}");
-                return;
-            }
+    void GenerateAddition(int min, int max)
+    {
+        Number1 = GenerateRandomDecimal(min, max);
+        Number2 = GenerateRandomDecimal(min, max);
+        Answer = Number1 + Number2;
+    }
 
-            attempts++;
-        }
+    void GenerateSubtraction(int min, int max)
+    {
+        Number1 = GenerateRandomDecimal(min, max);
+        Number2 = GenerateRandomDecimal(min, max);
+        Answer = Number1 - Number2;
+    }
 
-        GenerateFallback(min, max);
+    void GenerateMultiplication(int min, int max)
+    {
+        Number1 = GenerateRandomDecimal(1, 20);
+        Number2 = GenerateRandomDecimal(1, 20);
+        Answer = Number1 * Number2;
     }
 
     void GenerateSmartDivision(int min, int max)
     {
-        int resultDecimals = 3;
+        int resultDecimals = UnityEngine.Random.Range(1, 6);
 
         if (GameSettings.Instance != null &&
             GameSettings.Instance.validationMode == ValidationMode.Truncated)
         {
-            resultDecimals = GameSettings.Instance.decimals + 1;
+            int minRequired = GameSettings.Instance.decimals + 1;
+            resultDecimals = UnityEngine.Random.Range(minRequired, 7);
         }
 
-        decimal resultMultiplier = (decimal)Math.Pow(10, resultDecimals);
+        decimal multiplier = Pow10[resultDecimals];
 
-        // Generate target result
-        decimal result = UnityEngine.Random.Range(100, 9000) / resultMultiplier;
+        int integerPart = UnityEngine.Random.Range(0, 100);
 
-        // Decide divisor type
-        int divisorType = UnityEngine.Random.Range(0, 3);
+        int decimalPart;
+        do
+        {
+            decimalPart = UnityEngine.Random.Range(1, (int)multiplier);
+        }
+        while (decimalPart % 10 == 0);
+
+        decimal result = integerPart + decimalPart / multiplier;
 
         decimal divisor;
 
+        int divisorType = UnityEngine.Random.Range(0, 3);
+
         switch (divisorType)
         {
-            // small integer
             case 0:
                 divisor = UnityEngine.Random.Range(2, 10);
                 break;
 
-            // large integer
             case 1:
                 divisor = UnityEngine.Random.Range(10, 50);
                 break;
 
-            // decimal divisor
             default:
-                int decimals = UnityEngine.Random.Range(1, 3);
-                decimal multiplier = (decimal)Math.Pow(10, decimals);
 
-                divisor = UnityEngine.Random.Range(20, 200) / multiplier;
+                int divisorDecimals = UnityEngine.Random.Range(1, 3);
+                decimal divMultiplier = Pow10[divisorDecimals];
+
+                divisor = UnityEngine.Random.Range(20, 200) / divMultiplier;
+
                 break;
         }
 
         decimal dividend = result * divisor;
 
-        // Limit dividend decimals (keep numbers clean)
-        dividend = LimitDecimals(dividend, 4);
+        dividend = LimitDecimals(dividend, 5);
 
         Number1 = dividend;
         Number2 = divisor;
@@ -101,105 +132,10 @@ public class MathExercise
 
         Debug.Log($"Generated Division: {Number1} ÷ {Number2} = {Answer}");
     }
+
     decimal LimitDecimals(decimal value, int maxDecimals)
     {
-        decimal multiplier = (decimal)Math.Pow(10, maxDecimals);
-
+        decimal multiplier = Pow10[maxDecimals];
         return Math.Round(value * multiplier) / multiplier;
-    }
-    void GenerateFallback(int min, int max)
-    {
-        decimal n1 = UnityEngine.Random.Range(min, max);
-        decimal n2 = UnityEngine.Random.Range(min, max);
-
-        if (operation == OperationType.Divide && n2 == 0)
-            n2 = 1;
-
-        Number1 = n1;
-        Number2 = n2;
-        Answer = Calculate(n1, n2);
-
-        Debug.Log("Fallback exercise generated");
-    }
-
-    decimal Calculate(decimal n1, decimal n2)
-    {
-        switch (operation)
-        {
-            case OperationType.Add:
-                return n1 + n2;
-
-            case OperationType.Subtract:
-                return n1 - n2;
-
-            case OperationType.Multiply:
-                return n1 * n2;
-
-            case OperationType.Divide:
-                return n1 / n2;
-        }
-
-        return 0;
-    }
-
-    bool IsValidResult(decimal result)
-    {
-        if (GameSettings.Instance == null)
-            return true;
-
-        int decimals = GetDecimalCount(result);
-
-        if (decimals > 6)
-            return false;
-
-        if (Math.Abs(result) > 10000)
-            return false;
-
-        if (GameSettings.Instance.validationMode == ValidationMode.Truncated)
-        {
-            int requiredDecimals = GameSettings.Instance.decimals + 1;
-
-            if (decimals < requiredDecimals)
-                return false;
-        }
-
-        return true;
-    }
-
-    decimal GenerateRandomDecimal(int minValue, int maxValue)
-    {
-        int decimals = UnityEngine.Random.Range(0, 6);
-
-        decimal multiplier = (decimal)Math.Pow(10, decimals);
-
-        int minInt = minValue * (int)multiplier;
-        int maxInt = maxValue * (int)multiplier;
-
-        int randomInt = UnityEngine.Random.Range(minInt, maxInt);
-
-        return randomInt / multiplier;
-    }
-
-    int GetDecimalCount(decimal number)
-    {
-        string text = number.ToString(CultureInfo.InvariantCulture);
-
-        if (!text.Contains("."))
-            return 0;
-
-        return text.Split('.')[1].Length;
-    }
-
-    string GetSymbol()
-    {
-        switch (operation)
-        {
-            case OperationType.Add: return "+";
-            case OperationType.Subtract: return "-";
-            case OperationType.Multiply: return "×";
-            case OperationType.Divide: return "÷";
-        }
-
-        return "?";
     }
 }
